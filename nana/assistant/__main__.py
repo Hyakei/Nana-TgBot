c8ef8f8a8403a2d0dc5dbeb7804be63a75082184
Ayra Hikari <AyraHikari@gmail.com>
2019-08-01 10:44:54 +0700

    Change support group

--- nana/assistant/__main__.py
+++ nana/assistant/__main__.py
@@ -158,6 +158,6 @@ def report_some_errors(client, query):
 	err = query.message.text
 	open("nana/cache/errors.txt", "w").write(err)
 	query.message.edit_reply_markup(reply_markup=None)
-	app.send_document("EmiliaOfficial", "nana/cache/errors.txt", caption=text)
+	app.send_document("AyraSupport", "nana/cache/errors.txt", caption=text)
 	os.remove("nana/cache/errors.txt")
 	client.answer_callback_query(query.id, "Report was sent!")
