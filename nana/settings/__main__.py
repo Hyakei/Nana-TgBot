6e858a38b2e1b230019d71346d3f5978a5568d2d
Ayra Hikari <AyraHikari@gmail.com>
2019-08-01 10:24:57 +0700

    Clean up for better performance

--- nana/settings/__main__.py
+++ nana/settings/__main__.py
@@ -97,32 +97,21 @@ def get_myself_btn(client, query):
 	except ConnectionError:
 		client.answer_callback_query(query.id, "Bot is currently turned off!", show_alert=True)
 		return
-	getphoto = client.get_profile_photos(me.id)
-	if len(getphoto) == 0:
-		getpp = None
+
+	if query.message.caption:
+		text = query.message.caption.markdown
 	else:
-		getpp = getphoto[0].file_id
+		text = query.message.text.markdown
 
-	text = "**ℹ️ Your profile:**\n"
-	text += "First name: {}\n".format(me.first_name)
-	if me.last_name:
-		text += "Last name: {}\n".format(me.last_name)
-	text += "User ID: `{}`\n".format(me.id)
-	if me.username:
-		text += "Username: @{}\n".format(me.username)
+	num = []
+	num.append("*"*len(me.phone_number))
 
-	currtext = query.message.text or query.message.caption
-	if "***" not in currtext.markdown.split("Phone number: `")[1].split("`")[0]:
-		num = []
-		num.append("*"*len(me.phone_number))
-		text += "Phone number: `{}`\n".format("".join(num))
+	if "***" not in text.split("Phone number: `")[1].split("`")[0]:
+		text = text.replace("Phone number: `{}`\n".format(me.phone_number), "Phone number: `{}`\n".format("".join(num)))
 		button = InlineKeyboardMarkup([[InlineKeyboardButton("Show phone number", callback_data="hide_number")]])
 	else:
-		text += "Phone number: `{}`\n".format(me.phone_number)
+		text = text.replace("Phone number: `{}`\n".format("".join(num)), "Phone number: `{}`\n".format(me.phone_number))
 		button = InlineKeyboardMarkup([[InlineKeyboardButton("Hide phone number", callback_data="hide_number")]])
-
-	text += "`Nana Version    : v{}`\n".format(USERBOT_VERSION)
-	text += "`Manager Version : v{}`".format(SETTINGSBOT_VERSION)
 
 	if query.message.caption:
 		query.message.edit_caption(caption=text, reply_markup=button)
